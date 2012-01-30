63c1d2aad1ebbf4456d429cd03dee424adb0e2b1
Ben Reuben <breuben@gmail.com>
2012-01-29 17:00:58 -0800

    Proper passing of all headers between client and remote server.
    Added event handler for upgrading the connection for SSL
    (although it doesn't do anything yet).

--- proxy.js
+++ proxy.js
@@ -3,16 +3,44 @@ var https = require("https");
 var url = require("url");
 var fs = require("fs");
 
+function writeLog(message)
+{
+	message += "\n";
+	var path = "log.txt";
+	var logFile = fs.openSync(path, "a");
+	fs.writeSync(logFile, message, 0, null);
+	fs.closeSync(logFile);
+}
+
+function getPort(uri)
+{
+	if (uri.port != undefined)
+		return uri.port;
+	else if (uri.protocol == "https")
+		return 443;
+	else
+		return 80;
+}
+
 function makeRequest(request, callback)
 {
+	var requestUrl = url.parse(request.url);
+
 	var options = {
 		host: request.headers.host,
-		path: request.url,
+		port: getPort(requestUrl),
+		path: requestUrl.pathname,
 		method: request.method,
 		headers: request.headers
 	}
 
-	return http.request(options, callback);
+	delete options.headers.host;
+	delete options.headers["proxy-connection"];
+
+	if (requestUrl.protocol == "https")
+		return https.request(options, callback);
+	else
+		return http.request(options, callback);
 }
 
 function onStart(localRequest, localResponse)
@@ -34,7 +62,8 @@ function onStart(localRequest, localResponse)
 
 	function requestComplete(remoteResponse)
 	{
-		localResponse.writeHead(remoteResponse.statusCode);
+		localResponse.writeHead(remoteResponse.statusCode, remoteResponse.headers);
+
 		remoteResponse.on('data', function(chunk)
 		{
 			localResponse.write(chunk);
@@ -55,5 +84,17 @@ function onStart(localRequest, localResponse)
 }
 
 var server = http.createServer(onStart);
+
+server.on("upgrade", function(socket)
+{
+	console.log("socket");
+});
+
+server.on("upgrade", function(request, socket, head)
+{
+	console.log("upgrade");
+	socket.end();
+});
+
 server.listen(7777);
 
